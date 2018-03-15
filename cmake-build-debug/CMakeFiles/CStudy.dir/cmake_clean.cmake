826ec01ecab21f635b5cc0e79f4df4b976318be7
DESKTOP-91UIRR6\LinXunCheng <1573536876@qq.com>
2018-03-15 22:27:29 +0800

    初始化工程

---
+++ cmake-build-debug/CMakeFiles/CStudy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CStudy.dir/main.c.obj"
+  "CStudy.pdb"
+  "CStudy.exe"
+  "CStudy.exe.manifest"
+  "libCStudy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CStudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
