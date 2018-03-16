a8b64bccb9f6d3141716f1f71e3221c1b0457267
DESKTOP-91UIRR6\LinXunCheng <1573536876@qq.com>
2018-03-17 00:07:00 +0800

    第二次修改

--- main.c
+++ main.c
@@ -28,9 +28,43 @@ int main() {
     printf("num2的值是:%d\t num2的地址是:%p\n", num2, ptr2);
     return 0;
 }*/
-void main()
+/*void main()
 {
     int a=5, b=10;
     printf("%d\n", (a+b)/2);
+    system("pause");
+}
+*/
+void main()
+{
+    int sum = 0;
+    char ch;
+    printf("enter:");
+    while ((ch = getchar()) != '\n')
+        switch (ch) {
+            case 'A':case'E':case'I':case'L':case'N': case'O': case'R':case'S':case'T':case'U':
+                sum = sum + 1;
+                break;
+            case'D':case'G':
+                sum = sum + 2;
+                break;
+            case'B':case'C':case'M':case'P':
+                sum = sum + 3;
+                break;
+            case'F':case'H':case'V':case'W':case'Y':
+                sum = sum + 4;
+                break;
+            case'K':
+                sum = sum + 5;
+                break;
+            case'J':case'X':
+                sum = sum +8;
+                break;
+            case'Q':case'Z':
+                sum = sum + 10;
+                break;
+        }
+    printf("Scrabble value:%d\n", sum);
+    printf("\n");
     system("pause");
 }
